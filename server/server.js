fab35ecf1ad0f5ea48ba720e59710014178865e4
TristinRohr <161913577+TristinRohr@users.noreply.github.com>
2024-09-09 20:45:49 -0400

    fixed config and server

--- server/server.js
+++ server/server.js
@@ -53,9 +53,6 @@ async function startServer() {
 startServer();
 
 // MongoDB connection
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.log('MongoDB connection error:', err));

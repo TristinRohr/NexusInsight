9d8a86404102769e76835220b5ee0835de70c33c
TristinRohr <161913577+TristinRohr@users.noreply.github.com>
2024-09-13 20:33:39 -0400

    fixed render build command

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import * as dotenv from 'dotenv';
-
-// Load .env file manually
-dotenv.config({ path: './.env' });
-
-// Use the loaded environment variables
-const API_PORT = process.env.VITE_API_PORT || 3001; // Backend API port
-const VITE_PORT = process.env.VITE_PORT || 3000;    // Frontend port for Vite dev server
-
-
-console.log('Vite is running on port:', VITE_PORT); // Add this for debugging
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: VITE_PORT, // Frontend server port
     proxy: {
       '/graphql': {
-        target: `http://localhost:${API_PORT}`, // Backend API target
-        changeOrigin: true,
+        target: 'http://localhost:3001',
+        changeOrigin: true, // Ensure this is inside the proxy object
       },
     },
   },

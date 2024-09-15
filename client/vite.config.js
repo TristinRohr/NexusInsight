340be433826c8568d3973f515deabac654b39d9b
TristinRohr <161913577+TristinRohr@users.noreply.github.com>
2024-09-15 11:33:35 -0400

    trying to fix bugs

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,9 +4,12 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3000,
+    open: true,
     proxy: {
       '/graphql': {
         target: 'http://localhost:3001',
+        secure: false,
         changeOrigin: true, // Ensure this is inside the proxy object
       },
     },
